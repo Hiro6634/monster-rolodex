6a5efb42d6704310e63ae8f6012ba14e4d88c843
suyamaed <suyama_eduardo@redlink.com.ar>
2021-07-13 00:40:42 -0300

    30. Fetching Content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React,  { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import reactDom from 'react-dom';
@@ -8,22 +8,16 @@ class App extends Component{
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id:'aw1'
-        },
-        {
-          name: 'Dracula',
-          id: 'sw2'
-        },
-        {
-          name: 'Zombie',
-          id:'ak1'
-        }
-      ]
+      monsters: []
     };
   }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then( users => this.setState({monsters: users}));
+  }
+
   render() {
     return(
       <div className="App">
